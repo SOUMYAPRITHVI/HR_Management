b5b60c738460a16a31ebdee19ea2ad2c7f0d0e77
soumya <soumyakn89@gmail.com>
2023-11-13 16:58:10 +0530

    Add generate output function
    *create a folder
    *create visiting cards inside this folder with file name as firstletter of firstname with lastname
    *vcf format file created

--- gen_vcard.py
+++ gen_vcard.py
@@ -16,5 +16,18 @@ EMAIL;PREF;INTERNET:{email}
 REV:20150922T195243Z
 END:VCARD
 """
-
+def generate_output(op_directory='V_cards'):
+    if not os.path.exists(op_directory):
+        os.makedirs(op_directory)
+    with open('names.csv',newline='') as f:
+        row = csv.reader(f)
+        for r in row:
+            lname,fname,title,email,phone=r
+            vcard=create_vcard(lname,fname,title,email,phone)
+            filename=f'{fname[:1]}{lname}.vcf'
+            vcard_path=os.path.join(op_directory,filename)
+            with open(vcard_path, 'w') as f:
+                f.write(vcard)
+if __name__=="__main__":
+    generate_output()
 
