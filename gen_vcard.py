4ddf6e0304e4fd1e6a63e8d66fedf7553b923cb6
soumya <soumyakn89@gmail.com>
2023-11-22 23:52:38 +0530

    Import data from employee file to db
    *Add subparser command for import
    *Add handle_import functin
    *Implement subparser in main function

--- gen_vcard.py
+++ gen_vcard.py
@@ -5,19 +5,25 @@ import os
 import psycopg2 
 import requests
 import shutil
+import sys
 
-logger=None
+class HRException(Exception):pass
+
+logger=False
 
 def parse_args():
     parser=argparse.ArgumentParser(prog="gen_vcard.py",description="Generates sample names")
-    parser.add_argument("ipfile",help="Name of the input file")
-    parser.add_argument("opfile",help="Name of the output file")
+    # parser.add_argument("ipfile",help="Name of the input file")
+    # parser.add_argument("opfile",help="Name of the output file")
     parser.add_argument("-b","--dbname",help="Name of the database",default='HRmgt')
     parser.add_argument("-u","--dbuser",help="Username of the database")
-    
+    # subcommand initdb
     subparsers = parser.add_subparsers(dest="op")
     subparsers.add_parser("initdb", help="initialise the database")
-    
+    # import csv
+    import_parser = subparsers.add_parser("import", help="Import data from csv file")
+    import_parser.add_argument("employees_file", help="List of employees to import")
+   
     parser.add_argument("-i","--input_type",help="Specify the data source",choices=['file','db'],required=True)
     parser.add_argument("-v", "--verbose", help="Print detailed logging", action='store_true', default=False)
     parser.add_argument("-n", "--number", help="Number of records to generate", action='store', type=int, default=10)
@@ -40,8 +46,7 @@ def setup_logging(is_verbose):
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
    
-def create_vcard(row,address):
-    lname,fname,title,email,phone=row
+def generate_vcard(lname,fname,title,email,phone):
     return f"""
 BEGIN:VCARD
 VERSION:2.1
@@ -50,35 +55,11 @@ FN:{fname} {lname}
 ORG:Authors, Inc.
 TITLE:{title}
 TEL;WORK;VOICE:{phone}
-ADR;WORK:;;{address}
+ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
 EMAIL;PREF;INTERNET:{email}
 REV:20150922T195243Z
 END:VCARD
 """
-
-def file_exists(filename):
-    if not os.path.exists(filename) or not os.path.isfile(filename):
-        logger.error("%s input file not exists",filename)
-        exit(1)
-
-def clear_output_dir(args):
-    if os.path.exists(args.opfile):
-        shutil.rmtree(os.path.join(args.opfile))
-        return "Folder removed"
-    
-def read_input_csv(csvfile):
-    ret=[]
-    with open(csvfile,newline='') as f:
-        row = csv.reader(f)
-        for r in row:
-            ret.append(r)
-    return ret
-
-def parse_input_csv(row,args):
-    lname,fname,title,email,phone=row
-    vcard=create_vcard(lname,fname,title,email,phone,args.address)
-    filename=f'{fname[:1]}{lname}.vcf'
-    return filename,vcard
 
 def create_vcard_file(row,vcard,args):
     vcard_path=os.path.join(args.opfile,f"{str(row[2][:1])}{str(row[1])}.vcf")
@@ -109,23 +90,41 @@ def initialize_db(args):
         cur.execute(sql)
         con.commit()
     except psycopg2.OperationalError as e:
-        logger.info(f"Database '{args.dbname}' doesn't exist")
-      
-def main():
-    args=parse_args()
-    file_exists(args.ipfile) #checks if file exists
-    args = parse_args()
-    setup_logging(args.verbose)
-    ops = {"initdb" : initialize_db,
-            
-            }
-    ops[args.op](args)
-    data_from_db=fetch_values()
-    if data_from_db:
-        generate_output_from_db(data_from_db,args)
-    else:
-        logger.error("Failed to fetch database.")
+        raise HRException(f"Database '{args.dbname}' doesn't exist")
+     
+def handle_import(args):
+    con=psycopg2.connect(dbname=args.dbname)
+    cur=con.cursor()
+    cur.execute("truncate table employees restart identity cascade")
+    try:
+        with open(args.employees_file) as f:
+            reader=csv.reader(f)
+            for row in reader:
+                psql="insert into employees(fname,lname,title,email,phone) values(%s,%s,%s,%s,%s)"
+                cur.execute(psql,row[:5])
+        con.commit()
+        print("Values inserted ")
+    except:
+        con.rollback()
+        print("Values not inserted ")
+    finally:
+        cur.close()
+        con.close() 
 
+
+def main():
+    try:
+        args = parse_args()
+        setup_logging(args.verbose)
+        ops = {"initdb" : initialize_db,
+                "import" : handle_import,
+                
+                }
+        
+        ops[args.op](args)
+    except HRException as e:
+        logger.error("Program aborted, %s", e)
+        sys.exit(-1)
 if __name__=="__main__":
     main()
     
