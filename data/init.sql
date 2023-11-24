5af51b8f67ed72b4a651d18ab0511450b619263f
soumya <soumyakn89@gmail.com>
2023-11-24 10:50:39 +0530

    update init.sql file

--- data/init.sql
+++ data/init.sql
@@ -5,21 +5,33 @@ BEGIN
                 employee_id SERIAL PRIMARY KEY,
                 fname VARCHAR(50) NOT NULL,
                 lname VARCHAR(50) NOT NULL,
-                title VARCHAR(100),
                 email VARCHAR(120) NOT NULL,
-                phone VARCHAR(50)
+                phone VARCHAR(50),
+                title_id INTEGER REFERENCES designation(designation_id) ON DELETE CASCADE
             );
     END IF;
 END $$;
+
 DO $$ 
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'leaves') THEN
         CREATE TABLE leaves (
             id SERIAL PRIMARY KEY,
             date DATE,
-            employee  INTEGER REFERENCES employees(employee_id),
+            employee  INTEGER REFERENCES employees(employee_id) ON DELETE CASCADE,
             reason VARCHAR(200),
             UNIQUE (employee, date) -- An employee can take only one leave on a given day
+            );
+     END IF;
+END $$;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'designation') THEN
+        CREATE TABLE designation (
+            designation_id SERIAL PRIMARY KEY,
+            designation_name VARCHAR(100) NOT NULL,
+            percentage_of_employees  INTEGER NOT NULL,
+            total_no_of_leaves INTEGER NOT NULL
             );
      END IF;
 END $$;
