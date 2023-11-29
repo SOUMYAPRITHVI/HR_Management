22ee80d7315c96b6f9d45165c96e6331337c390e
soumya <soumyakn89@gmail.com>
2023-11-29 17:25:43 +0530

    Update different function
    *create function for single employee selection
    *create function for multiple employee selection

--- gen_vcard.py
+++ gen_vcard.py
@@ -1,32 +1,46 @@
 import argparse
 import csv
 import logging
+import datetime
 import os 
+import sys
+import shutil
+
+import configparser
 import psycopg2 
 import requests
-import sys
+
 
 class HRException(Exception):pass
 
 logger=False
+todays_date = str(datetime.date.today())
 
 def parse_args():
     parser=argparse.ArgumentParser(prog="gen_vcard.py",description="Employee information manager for a small company.")
-    # parser.add_argument("ipfile",help="Name of the input file")
-    # parser.add_argument("opfile",help="Name of the output file")
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    # parser.add_argument("--dbname", help="Adding database name", action="store", type=str, default=config.get('Database', 'dbname'))
     parser.add_argument("-b","--dbname",help="Name of the database",default='HRmgt')
-    parser.add_argument("-u","--dbuser",help="Username of the database")
+    parser.add_argument("--ipfile",help="Name of the input file as csv", default="names.csv")
+    parser.add_argument("--opfile",help="Name of the output Folder", default="Vcard")
     # subcommand initdb
     subparsers = parser.add_subparsers(dest="op")
     subparsers.add_parser("initdb", help="Initialise the database")
-    
     # import csv
     import_parser = subparsers.add_parser("import", help="Import data from csv file")
     import_parser.add_argument("employees_file", help="List of employees to import")
+    # create vcards of each person
+    vcard_parser=subparsers.add_parser("vcard",help="Generate vcard of each record")
+    vcard_parser.add_argument("--qrcode",help="Generate QRCODE of each record",action='store_true',default=False)
+    vcard_parser.add_argument("--addres", help="Change into new address",type=str, default="100 Flat Grape Dr.;Fresno;CA;95555;United States of America")
     # fetch vcard
     query_parser = subparsers.add_parser("query", help="Get information for a single employee")
-    query_parser.add_argument("--vcard", action="store_true", default=False, help="Generate vcard for employee")
+    query_parser.add_argument("--displayvcard", help="Generate vcard for employee", action="store_true", default=False)
     query_parser.add_argument("id", help="Employee id")
+    query_parser.add_argument("-q", "--qrcodee", help="Generate QRCODE of single person",action='store_true',default=False)
+    query_parser.add_argument("--opfile",help="Name of the output Folder", default="Vcard")
+    # query_parser.add_argument("-d", "--dimension", help="Change dimension of QRCODE",action='store_true', type=int, default=200 )
     # add leaves
     leave_parser = subparsers.add_parser("leave", help="Add leave to database")
     leave_parser.add_argument("date", type=str, help="Date of absence")
@@ -39,11 +53,14 @@ def parse_args():
     parser_export = subparsers.add_parser("export", help="Export leave summary")
     parser_export.add_argument("directory", help="Directory to export leave summary")
  
-    parser.add_argument("-i","--input_type",help="Specify the data source",choices=['file','db'],required=True)
+    # parser.add_argument("-i","--input_type",help="Specify the data source",choices=['file','db'],required=True)
+    
     parser.add_argument("-v", "--verbose", help="Print detailed logging", action='store_true', default=False)
+    # parser.add_argument("--vcard", help="Generate vcard of each record", action='store_true', default=True)
     parser.add_argument("-n", "--number", help="Number of records to generate", action='store', type=int, default=10)
     parser.add_argument("-d", "--dimension", help="Change dimension of QRCODE", default=200 )
-    parser.add_argument("-q", "--add_qr", help="Generate QRCODE of each record",action='store_true',default=False)
+    # parser.add_argument("-q", "--qrcodee", help="Generate QRCODE of each record",action='store_true',default=False)
+    parser.add_argument("-a", "--address", help="Change into new address",type=str, default="100 Flat Grape Dr.;Fresno;CA;95555;United States of America")
     args=parser.parse_args()
     return args
 
@@ -59,8 +76,16 @@ def setup_logging(is_verbose):
     handler.setFormatter(logging.Formatter("[%(levelname)s]| %(filename)s:%(lineno)d | %(message)s"))
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
-   
-def generate_vcard(lname,fname,title,email,phone):
+
+def update_config(dbname):
+  config = configparser.ConfigParser()
+  config.read('config.ini')
+  config.set('Database','dbname',dbname)
+  with open('config.ini','w') as config_file:
+     config.write(config_file)
+
+def create_vcard(row,address):
+    lname,fname,title,email,phone=row
     return f"""
 BEGIN:VCARD
 VERSION:2.1
@@ -69,32 +94,29 @@ FN:{fname} {lname}
 ORG:Authors, Inc.
 TITLE:{title}
 TEL;WORK;VOICE:{phone}
-ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
+ADR;WORK:;;{address}
 EMAIL;PREF;INTERNET:{email}
 REV:20150922T195243Z
 END:VCARD
 """
 
-def create_vcard_file(row,vcard,args):
-    vcard_path=os.path.join(args.opfile,f"{str(row[2][:1])}{str(row[1])}.vcf")
-    with open(vcard_path, "w") as file:
-        file.write(vcard)
    
-def create_qr_code(row,vcard,args):
-    url=f"https://chart.googleapis.com/chart?cht=qr&chs={args.dimension}x{args.dimension}&chl={args.email}"
-    resp=requests.get(url)
-    qr_path=os.path.join(args.opfile, f"{str(row[2][:1])}{str(row[1])}.qr.png")
+def create_qr_code(vcard,args,row):
+    print(args)
+    url=f"https://chart.googleapis.com/chart?cht=qr&chs={args.dimension}x{args.dimension}&chl={vcard}"
+    resp=requests.get(url).content
+    qr_path=os.path.join(args.opfile, f"{str(row[1][:1])}{str(row[0])}.qr.png")
     if os.path.exists(qr_path):
         logger.warning(f"File already exists: {qr_path}")
     else:
-        if os.access(args.opfile, os.W_OK):
-            with open(qr_path, "wb") as file:
-                file.write(url.content)
-                logger.info(f"Created QR code: {qr_path}")
-        else:
-            logger.warning(f"No write access to directory: {args.opfile}")
+        with open(qr_path, "wb") as file:
+            file.write(resp)
+            logger.debug(f"Created QR code: {qr_path}")
+    # else:
+            # logger.warning(f"No write access to directory: {args.opfile}")
          
 def initialize_db(args):
+    update_config(args.dbname)
     with open("data/init.sql") as f:
         sql=f.read()
         logger.debug(sql)
@@ -126,24 +148,84 @@ def handle_import(args):
         logging.info("Values not inserted ")
     finally:
         con.close() 
-def fetch_from_db(args):
+def create_vcard_file(row,vcard,args):
+    vcard_path=os.path.join(args.opfile,f"{str(row[1][:1])}{str(row[0])}.vcf")
+    with open(vcard_path, "w") as file:
+        file.write(vcard)
+def clear_output_dir(args):
+    if os.path.exists(args.opfile):
+        shutil.rmtree(os.path.join(args.opfile))
+        return "Folder removed"
+def generate_multiple_vcards_from_db(emplyee_data,args):
+    count=1
+    if not os.path.exists(args.opfile):
+        os.makedirs(args.opfile)
+        if args.qrcode:
+            logger.info("QR code generation started,Take few minutes to complete.")
+        for row in emplyee_data[:args.number]:
+            vcard=create_vcard(row[:5],args.addres)
+            create_vcard_file(row[:5],vcard,args)
+            if args.qrcode:
+                create_qr_code(vcard,args,row[:2])
+            if count==args.number:
+                break
+            count +=1
+            logger.debug(f"Created the file of {row[:1]} ")
+        logger.info(f"Successfully created the first {count} person files")
+    else:
+        logger.warning("The output folder already exists,If you want to Delete press (y/n): ")
+        warn=input()
+        if warn=="y":
+            logger.info( clear_output_dir(args))
+        else:
+            logger.info("Output directory not removed" )
+def create_vcard_from_db(args):
     try:
         with psycopg2.connect(dbname=args.dbname) as con:
             with con.cursor() as cur:        
-                query=f"select e.fname, e.lname, e.email, e.phone,d.designation_name  from employees e  INNER JOIN designation d ON e.title_id = d.designation_id where e.employee_id={args.id}"
+                query=f"select e.fname, e.lname, e.email, e.phone,d.designation_name  from employees e  INNER JOIN designation d ON e.title_id = d.designation_id"
                 cur.execute(query)
-                fname, lname, email, phone ,designation= cur.fetchone()
-                print (f"""Name        : {fname} {lname}
-            Designation : {designation}
-            Email       : {email}
-            Phone       : {phone}""")
-                if (args.vcard):
-                    vcard = generate_vcard(lname, fname, designation, email, phone)
-                    print (f"\n{vcard}")
+                data=[list(row) for row in cur.fetchall()]
+                generate_multiple_vcards_from_db(data,args)
     except psycopg2.Error as e:
                 logging.info(f"Database error: {e}")
-    finally:
-        con.close()
+def get_single_person_from_db(args):
+    try:
+        with psycopg2.connect(dbname=args.dbname) as con:
+            with con.cursor() as cur:       
+                query=f"select e.fname, e.lname, e.email, e.phone,d.designation_name  from employees e  inner JOIN designation d ON e.title_id = d.designation_id where e.employee_id={args.id}"
+                cur.execute(query)
+                fname, lname, email, phone ,designation= cur.fetchone()
+                print (f"""Name        : {fname} {lname}\nDesignation : {designation}\nEmail       : {email}\nPhone       : {phone}""")
+                if (args.displayvcard):
+                    data=lname, fname, designation, email, phone
+                    vcard = create_vcard(data,args.address)
+                    print (f"\n{vcard}")
+                if args.qrcodee:
+                    print(args)
+                    desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+                    # Create the full path to the new folder
+                    new_folder_path = os.path.join(desktop_path, args.opfile)
+                    if not os.path.exists(new_folder_path):
+                        os.makedirs(new_folder_path)
+                        url=f"https://chart.googleapis.com/chart?cht=qr&chs={args.dimension}x{args.dimension}&chl={vcard}"
+                        resp=requests.get(url).content
+                        qr_path=os.path.join(new_folder_path, f"{str(data[1][:1])}{str(data[0])}.qr.png")
+                        with open(qr_path, "wb") as file:
+                            file.write(resp)
+                        logger.info("QR code generated in user specified folder in desktop")
+                    else:
+                        logger.warning("The output folder already exists,If you want to Delete press (y/n): ")
+                        warn=input()
+                        if warn=="y":
+                            shutil.rmtree(os.path.join(new_folder_path))
+                            logger.info("Output directory removed from desktop" )
+                        else:
+                            logger.info("Output directory not removed" )
+       
+    except psycopg2.Error as e:
+                logging.info(f"Database error: {e}")
+  
 
 def insert_leaves(args):
     try:
@@ -248,16 +330,18 @@ def main():
         setup_logging(args.verbose)
         ops = {"initdb" : initialize_db,
                 "import" : handle_import,
-                "query" : fetch_from_db,
+                "query" : get_single_person_from_db,
+                "vcard" : create_vcard_from_db,
                 "leave" : insert_leaves, 
                 "count": count_of_leaves,
                 "export":export_leave_summary
                 }
         
-        ops[args.op](args)
+        ops[args.op](args)       
     except HRException as e:
         logger.error("Program aborted, %s", e)
         sys.exit(-1)
+
 if __name__=="__main__":
     main()
     
